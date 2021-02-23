81d9bf2822bccaf2b676f9e8ddf8cbf9ad8c6bdd
Illya <example@gmail.com>
2021-02-23 16:23:03 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/* alert('Hello!'); */
+
+/* const result = confirm("Are you here?");
+console.log(result); */
+
+
+/* const answer = +prompt("18+?", "18");
+console.log(answer + 5); */
+
+
+/*  const answers = []; */
+
+/* answers[0] = prompt('What`s you name?', '');
+answers[1] = prompt('What`s you Family?', '');
+answers[2] = prompt('What`s your age?', '');  */
+
+/* console.log(typeof(answers)); */
+
+/* const category = 'toys';
+console.log(`https://someurl.com/${category}/5`); */
+
+/* const user = 'Ivan';
+alert(`Hello, ${user}`); */
+
+
+
+/* console.log('arr' + " - object");
+console.log(4 + "5"); */
+
+let incr = 10,
+    decr = 10;
+
+/* ++incr; 
+--decr;  */
+
+// console.log(incr++);
+// console.log(decr--);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+const isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
+
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert(+a + +b); 
