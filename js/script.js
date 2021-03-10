d033d3ca0a36721b3c7278b581af34e5f13ad622
Illya <example@gmail.com>
2021-03-10 18:31:55 +0200

    practice lessons 12

--- js/script.js
+++ js/script.js
@@ -1,53 +1,60 @@
-"use strict";
+                                    ////// Lesson 12 //////
+/* Задание на урок:
 
-/* alert('Hello!'); */
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-/* const result = confirm("Are you here?");
-console.log(result); */
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
 
-/* const answer = +prompt("18+?", "18");
-console.log(answer + 5); */
+                                    ////// Lesson  //////
+/* Задание на урок:
 
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-/*  const answers = []; */
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-/* answers[0] = prompt('What`s you name?', '');
-answers[1] = prompt('What`s you Family?', '');
-answers[2] = prompt('What`s your age?', '');  */
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-/* console.log(typeof(answers)); */
+4) Потренироваться и переписать цикл еще двумя способами*/
 
-/* const category = 'toys';
-console.log(`https://someurl.com/${category}/5`); */
-
-/* const user = 'Ivan';
-alert(`Hello, ${user}`); */
-
-
-
-/* console.log('arr' + " - object");
-console.log(4 + "5"); */
-
-let incr = 10,
-    decr = 10;
-
-/* ++incr; 
---decr;  */
-
-// console.log(incr++);
-// console.log(decr--);
-
-console.log(5%2);
-
-console.log(2*4 == 8);
-
-const isChecked = true,
-    isClose = true;
-
-console.log(isChecked && isClose);
-
-let a = prompt("Первое число?", 1);
-let b = prompt("Второе число?", 2);
-
-alert(+a + +b); 
